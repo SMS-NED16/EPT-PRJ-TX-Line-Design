31463da4a81abb013796d7d6a01729c20ad997cf
Saad Siddiqui <saadsiddiqui.ned16@gmail.com>
2019-06-06 17:24:31 +0500

    Aiwein ki commit

---
+++ oop-tutorials/oop_scratchpad.m
@@ -0,0 +1,13 @@
+% Creating a struct
+S(1).Name = 'Fred Flintsone';
+S(1).SID = 1234567;
+S(2).Name = 'Barney Ruble';
+S(2).SID = 234512;
+S(3).Name = 'Bam Bam';
+S(3).SID = 345617;
+
+S_1.name = 'Fred Flintsone'; S_1.SID = 1234567;
+S_2.name = 'Barney Ruble'; S_2.SID = 234512;
+S_3.name = 'Bam Bam'; S_3_.SID = 345617;
+
+struct_array = [S_1, S_2, S_3]
